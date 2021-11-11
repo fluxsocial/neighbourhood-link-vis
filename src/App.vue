3337912031a5f5aa78290aa857e972cc4f993ed1
jdeepee <joshuadparkin@gmail.com>
2021-11-11 11:48:30 +0000

    get perspective meta data loading

--- src/App.vue
+++ src/App.vue
@@ -13,24 +13,98 @@
 <script>
 import "vue-vis-network/node_modules/vis-network/dist/vis-network.css";
 import { ad4mClient } from "./main";
+import { v4 as uuidv4 } from 'uuid';
 
 export default {
   name: 'App',
   methods: {
     async load() {
-      this.nodes = await this.getNodes();
+      const [nodes, edges] = await this.getPerspectiveNodesAndMetaEdges();
+      this.nodes = nodes;
+      this.edges = edges;
     },
-    async getNodes() {
-      console.log("getNodes()");
+    async getPerspectiveNodesAndMetaEdges() {
+      console.log("getPerspectiveNodesAndMetaEdges()");
       const perspectives = await ad4mClient.perspective.all();
       console.log(perspectives);
-      return perspectives.map((perspective) => {
-        console.log(perspective);
-        return {
+      let nodes = [];
+      let edges = [];
+      for (const perspective of perspectives) {
+        const neighbourhoodLinks = perspective.neighbourhood.meta.links;
+        const neighbourhoodLanguage = perspective.neighbourhood.linkLanguage;
+        //Create perspective node
+        const perspectiveData = {
           id: perspective.uuid,
-          label: perspective.name,
+          label: perspective.name + "Neighbourhood",
+          widthConstraint: 100,
+          shape: 'database'
+        };
+        //Create language node
+        const neighbourhoodLanguageData = {
+          id: neighbourhoodLanguage,
+          label: neighbourhoodLanguage + " Link Language",
+          widthConstraint: 100
         }
-      })
+        nodes.push(perspectiveData);
+        nodes.push(neighbourhoodLanguageData);
+        const metaLinkNode = uuidv4();
+        const metaLinks = {
+          id: metaLinkNode,
+          label: "metaLinks",
+          shape: 'database'
+        }
+        nodes.push(metaLinks);
+        edges.push({
+          from: perspective.uuid,
+          to: metaLinkNode,
+          label: "metaLinks"
+        })
+        //Add the meta links to the network
+        for (const metaLink of neighbourhoodLinks) {
+          const link = metaLink.data;
+          let edge = {}
+          //Use unique edges here
+          const id = uuidv4();
+          //If source self then use from as perspective
+          if (link.source == "self") {
+            edge = {
+              from: metaLinkNode,
+              to: id,
+              label: link.predicate
+            } 
+          } else {
+            edge = {
+              from: link.source,
+              to: link.target,
+              label: link.predicate
+            }
+          }
+          //Add a node for target data
+          //We always add a node here since we dont want perspectives to share link targets
+          nodes.push({
+            id: id,
+            label: link.target,
+            widthConstraint: 100
+          })
+          edges.push(edge)
+        }
+        //Now start to look for the actual links on the link language
+        const linkLanguageLinksNode = uuidv4();
+        const linkLanguageLinks = {
+          id: linkLanguageLinksNode,
+          label: "linkLanguageLinks",
+          shape: 'database'
+        }
+        nodes.push(linkLanguageLinks);
+        edges.push({
+          from: perspective.uuid,
+          to: linkLanguageLinksNode,
+          label: "linkLanguageLinks"
+        })
+        // const links = await ad4mClient.perspective.queryLinks(perspective.uuid, {limit: 10});
+        // console.log(links);
+      }
+      return [nodes, edges]
     }
   },
   data() {
@@ -38,11 +112,38 @@ export default {
       nodes: [],
       edges: [],
       options: {
-         nodes: {
-          borderWidth: 4
-         },
-         edges: {
-          color: 'lightgray'
+        autoResize: true,
+        nodes: {
+          borderWidth: 1
+        },
+        edges: {
+          color: 'grey',
+          smooth: {
+            type: "continous"
+          }
+        },
+        physics: {
+          barnesHut: {
+            springConstant: 0,
+            avoidOverlap: 0.2
+          },
+          enabled: true,
+          hierarchicalRepulsion: {
+              centralGravity: 0.0,
+              springLength: 500,
+              springConstant: 0.01,
+              nodeDistance: 300,
+              damping: 0.09
+          },
+          solver: 'hierarchicalRepulsion'
+        },
+        layout: {
+          hierarchical: {
+            direction: "UD",
+            sortMethod: "directed",
+            nodeSpacing: 150,
+            treeSpacing: 100,
+          }
         }
       }
     }
