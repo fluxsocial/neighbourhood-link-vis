e2dffd7e19e7cacb803c3e718e3ad9281ca98655
jdeepee <joshuadparkin@gmail.com>
2021-11-22 15:45:03 +0000

    cleanup code and get linkLanguage links rendering

--- src/App.vue
+++ src/App.vue
@@ -23,86 +23,158 @@ export default {
       this.nodes = nodes;
       this.edges = edges;
     },
+    loadPerspectiveNode(perspective) {
+      const perspectiveNode = {
+        id: perspective.uuid,
+        label: perspective.name + "-Perspective",
+        widthConstraint: 100,
+        shape: 'database'
+      };
+      return perspectiveNode;
+    },
+    loadNeighbourhoodNodes(perspective) {
+      const nodes = [];
+      const neighbourhoodLanguage = perspective.neighbourhood.linkLanguage;
+      //Create perspective node
+      const perspectiveNode = {
+        id: perspective.uuid,
+        label: perspective.name + "-Neighbourhood",
+        widthConstraint: 100,
+        shape: 'database'
+      };
+      //Create language node
+      const neighbourhoodLanugageNode = {
+        id: neighbourhoodLanguage,
+        label: neighbourhoodLanguage,
+        widthConstraint: 100
+      }
+      nodes.push(perspectiveNode);
+      nodes.push(neighbourhoodLanugageNode);
+      return [nodes, [{type: "perspective", id: perspective.uuid}, {type: "linkLanguage", id: neighbourhoodLanguage}]]
+    },
+    loadMetaLinks(perspective) {
+      const neighbourhoodMetaLinks = perspective.neighbourhood.meta.links;
+      const nodes = [];
+      const edges = [];
+      const metaLinkNode = uuidv4();
+      const metaLinks = {
+        id: metaLinkNode,
+        label: "metaLinks",
+        shape: 'database'
+      }
+      nodes.push(metaLinks);
+      edges.push({
+        from: perspective.uuid,
+        to: metaLinkNode,
+        label: "metaLinks"
+      })
+      //Add the meta links to the network
+      for (const metaLink of neighbourhoodMetaLinks) {
+        const link = metaLink.data;
+        let edge = {}
+        //Use unique edges here
+        const id = uuidv4();
+        //If source self then use from as perspective
+        if (link.source == "self") {
+          edge = {
+            from: metaLinkNode,
+            to: id,
+            label: link.predicate
+          } 
+        } else {
+          edge = {
+            from: link.source,
+            to: link.target,
+            label: link.predicate
+          }
+        }
+        //Add a node for target data
+        //We always add a node here since we dont want perspectives to share link targets
+        nodes.push({
+          id: id,
+          label: link.target,
+          widthConstraint: 100
+        })
+        edges.push(edge)
+      }
+
+      return [nodes, edges, [{type: "metaLinksDatabase", id: metaLinkNode}]]
+    },
+    async loadLinkLanguageLinks(perspective) {
+      const nodes = [];
+      const edges = [];
+      //Now start to look for the actual links on the link language
+      const linkLanguageLinksNode = uuidv4();
+      const linkLanguageLinks = {
+        id: linkLanguageLinksNode,
+        label: "linkLanguageLinks",
+        shape: 'database'
+      }
+      nodes.push(linkLanguageLinks);
+      edges.push({
+        from: perspective.uuid,
+        to: linkLanguageLinksNode,
+        label: "linkLanguageLinks"
+      })
+      edges.push({
+        from: linkLanguageLinksNode,
+        to: perspective.neighbourhood.linkLanguage,
+        label: "usesLanguage"
+      })
+      const links = await ad4mClient.perspective.queryLinks(perspective.uuid, {limit: 10});
+      
+      for (const link of links) {
+        const linkData = link.data;
+        console.log(linkData);
+        const sourceNode = {
+          id: linkData.source,
+          label: linkData.source,
+        }
+        const targetNode = {
+          id: linkData.target,
+          label: linkData.target
+        }
+        if (nodes.filter(node => node.id === linkData.source).length == 0) { nodes.push(sourceNode) }
+        if (nodes.filter(node => node.id === linkData.target).length == 0) { nodes.push(targetNode) }
+        edges.push({
+          from: linkLanguageLinksNode,
+          to: linkData.source,
+          label: "containsLink"
+        })
+        edges.push({
+          from: linkData.source,
+          to: linkData.target,
+          label: linkData.predicate
+        })
+      }
+      return [nodes, edges]
+    },
     async getPerspectiveNodesAndMetaEdges() {
       console.log("getPerspectiveNodesAndMetaEdges()");
       const perspectives = await ad4mClient.perspective.all();
-      console.log(perspectives);
+      console.log("Got perspectives: ", perspectives);
       let nodes = [];
       let edges = [];
       for (const perspective of perspectives) {
-        const neighbourhoodLinks = perspective.neighbourhood.meta.links;
-        const neighbourhoodLanguage = perspective.neighbourhood.linkLanguage;
-        //Create perspective node
-        const perspectiveData = {
-          id: perspective.uuid,
-          label: perspective.name + "Neighbourhood",
-          widthConstraint: 100,
-          shape: 'database'
-        };
-        //Create language node
-        const neighbourhoodLanguageData = {
-          id: neighbourhoodLanguage,
-          label: neighbourhoodLanguage + " Link Language",
-          widthConstraint: 100
+        if (perspective.neighbourhood) {
+          //Load the neighbourhood data
+          const [neighbourhoodNodes, nodeRefs] = this.loadNeighbourhoodNodes(perspective);
+          //Load the meta data
+          const [metaNodes, neighbourhoodEdges, metaNodeRefs] = this.loadMetaLinks(perspective);
+          //Add nodes and edges to output
+          nodes = nodes.concat(neighbourhoodNodes);
+          nodes = nodes.concat(metaNodes);
+          edges = edges.concat(neighbourhoodEdges);
+          //Keep one array of node refs we might need later
+          nodeRefs.concat(metaNodeRefs);
+
+          const [linkNodes, linkEdges] = await this.loadLinkLanguageLinks(perspective);
+          nodes = nodes.concat(linkNodes);
+          edges = edges.concat(linkEdges);
+        } else {
+          const perspectiveNode = this.loadPerspectiveNode(perspective);
+          nodes.push(perspectiveNode);
         }
-        nodes.push(perspectiveData);
-        nodes.push(neighbourhoodLanguageData);
-        const metaLinkNode = uuidv4();
-        const metaLinks = {
-          id: metaLinkNode,
-          label: "metaLinks",
-          shape: 'database'
-        }
-        nodes.push(metaLinks);
-        edges.push({
-          from: perspective.uuid,
-          to: metaLinkNode,
-          label: "metaLinks"
-        })
-        //Add the meta links to the network
-        for (const metaLink of neighbourhoodLinks) {
-          const link = metaLink.data;
-          let edge = {}
-          //Use unique edges here
-          const id = uuidv4();
-          //If source self then use from as perspective
-          if (link.source == "self") {
-            edge = {
-              from: metaLinkNode,
-              to: id,
-              label: link.predicate
-            } 
-          } else {
-            edge = {
-              from: link.source,
-              to: link.target,
-              label: link.predicate
-            }
-          }
-          //Add a node for target data
-          //We always add a node here since we dont want perspectives to share link targets
-          nodes.push({
-            id: id,
-            label: link.target,
-            widthConstraint: 100
-          })
-          edges.push(edge)
-        }
-        //Now start to look for the actual links on the link language
-        const linkLanguageLinksNode = uuidv4();
-        const linkLanguageLinks = {
-          id: linkLanguageLinksNode,
-          label: "linkLanguageLinks",
-          shape: 'database'
-        }
-        nodes.push(linkLanguageLinks);
-        edges.push({
-          from: perspective.uuid,
-          to: linkLanguageLinksNode,
-          label: "linkLanguageLinks"
-        })
-        // const links = await ad4mClient.perspective.queryLinks(perspective.uuid, {limit: 10});
-        // console.log(links);
       }
       return [nodes, edges]
     }
@@ -117,10 +189,7 @@ export default {
           borderWidth: 1
         },
         edges: {
-          color: 'grey',
-          smooth: {
-            type: "continous"
-          }
+          color: 'grey'
         },
         physics: {
           barnesHut: {
